0e11ad1f5a5294179738574f1d9348284e2e22f0
ranawatharsh <ranawatharsh06@gmail.com>
2024-02-10 19:07:14 +0530

    UI-4

--- FrontEnd/src/Components/Filters.jsx
+++ FrontEnd/src/Components/Filters.jsx
@@ -9,14 +9,14 @@ function Filters() {
     return (
         <div>
             <div className='flex flex-col md:flex-row justify-center items-center'>
-                <div className='mr-6 text-3xl pt-2 font-bold text-black'>Filters</div>
+                
                 <div className="w-72 mr-0 md:mr-10">
                     <label htmlFor="type" className="block text-sm font-medium text-black mb-1">Type</label>
                     <select
                         id="type"
                         value={typeState.type}
                         onChange={(e) => typeState.setType(e.target.value)}
-                        className="py-2 px-3 border border-gray-300 rounded-md w-full focus:outline-none focus:ring focus:border-blue-300"
+                        className="py-2 px-3 border border-gray-300 rounded-3xl w-full focus:outline-none focus:ring focus:border-blue-300"
                     >
                         <option value="restaurants">Restaurants</option>
                         <option value="hotels">Hotels</option>
@@ -30,15 +30,19 @@ function Filters() {
                         id="rating"
                         value={ratingState.rating}
                         onChange={(e) => ratingState.setRating(e.target.value)}
-                        className="py-2 px-3 border border-gray-300 rounded-md w-full focus:outline-none focus:ring focus:border-blue-300"
+                        className="py-2 px-3 border border-gray-300 rounded-3xl w-full focus:outline-none focus:ring focus:border-blue-300"
                     >
                         <option value="">All</option>
                         <option value="3">Above 3.0</option>
                         <option value="4">Above 4.0</option>
                         <option value="4.5">Above 4.5</option>
                     </select>
+                    
                 </div>
+                
+                
             </div>
+            <hr className='bg-blue-500 mt-8'></hr>
             <div className='flex justify-center items-center'>
                 <UpdateLocation />
             </div>
